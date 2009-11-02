22bd6cf8015d53811820a37b8d875286b123008e
pidder1 <pidder1@d18a777a-c15b-475a-a9be-688d43f1f219>
2009-11-02 15:08:31 +0000

    git-svn-id: svn://svn.code.sf.net/p/pidcrypt/code/trunk@51 d18a777a-c15b-475a-a9be-688d43f1f219

--- javascripts/aes_core.js
+++ javascripts/aes_core.js
@@ -1,16 +1,20 @@
 /*!Copyright (c) 2009 pidder <www.pidder.com>*/
 /*----------------------------------------------------------------------------*/
-// Permission to use, copy, modify, and/or distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+// 02111-1307 USA or check at http://www.gnu.org/licenses/gpl.html
+
 /*----------------------------------------------------------------------------*/
 /*
 *  pidCrypt AES core implementation for block en-/decryption for use in pidCrypt
@@ -25,20 +29,6 @@
 /*    http://point-at-infinity.org/jsaes/
  *    Report bugs to: jsaes AT point-at-infinity.org
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307 USA or check at http://www.gnu.org/licenses/gpl.html
  *
  * This is a javascript implementation of the AES block cipher. Key lengths
  * of 128, 192 and 256 bits are supported.
