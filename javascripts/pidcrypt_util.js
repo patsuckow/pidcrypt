ed704e8cc256864c9ab2763d90b5ad2fddc86de2
pidder1 <pidder1@d18a777a-c15b-475a-a9be-688d43f1f219>
2010-02-25 13:03:32 +0000

    git-svn-id: svn://svn.code.sf.net/p/pidcrypt/code/trunk@69 d18a777a-c15b-475a-a9be-688d43f1f219

--- javascripts/pidcrypt_util.js
+++ javascripts/pidcrypt_util.js
@@ -135,6 +135,38 @@ pidCryptUtil.encodeUTF8 = function(str) {
   return str;
 }
 
+// If you encounter problems with the UTF8 encode function (e.g. for use in a
+// Firefox) AddOn) you can use the following instead.
+// code from webtoolkit.com
+
+//pidCryptUtil.encodeUTF8 = function(str) {
+//		str = str.replace(/\r\n/g,"\n");
+//		var utftext = "";
+//
+//		for (var n = 0; n < str.length; n++) {
+//
+//			var c = str.charCodeAt(n);
+//
+//			if (c < 128) {
+//				utftext += String.fromCharCode(c);
+//			}
+//			else if((c > 127) && (c < 2048)) {
+//				utftext += String.fromCharCode((c >> 6) | 192);
+//				utftext += String.fromCharCode((c & 63) | 128);
+//			}
+//			else {
+//				utftext += String.fromCharCode((c >> 12) | 224);
+//				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+//				utftext += String.fromCharCode((c & 63) | 128);
+//			}
+//
+//		}
+//
+//  return utftext;
+//}
+
+
+
 /**
  * Decode utf-8 encoded string back into multi-byte Unicode characters
  *
@@ -156,6 +188,46 @@ pidCryptUtil.decodeUTF8 = function(str) {
     );
   return str;
 }
+
+// If you encounter problems with the UTF8 decode function (e.g. for use in a
+// Firefox) AddOn) you can use the following instead.
+// code from webtoolkit.com
+
+//pidCryptUtil.decodeUTF8 = function(utftext) {
+//    var str = "";
+//		var i = 0;
+//		var c = 0;
+//    var c1 = 0;
+//    var c2 = 0;
+//
+//		while ( i < utftext.length ) {
+//
+//			c = utftext.charCodeAt(i);
+//
+//			if (c < 128) {
+//				str += String.fromCharCode(c);
+//				i++;
+//			}
+//			else if((c > 191) && (c < 224)) {
+//				c1 = utftext.charCodeAt(i+1);
+//				str += String.fromCharCode(((c & 31) << 6) | (c1 & 63));
+//				i += 2;
+//			}
+//			else {
+//				c1 = utftext.charCodeAt(i+1);
+//				c2 = utftext.charCodeAt(i+2);
+//				str += String.fromCharCode(((c & 15) << 12) | ((c1 & 63) << 6) | (c2 & 63));
+//				i += 3;
+//			}
+//
+//		}
+//
+//
+//  return str;
+//}
+
+
+
 
 /**
  * Converts a string into a hexadecimal string
